3176c95c450e296baf0b2f4eabee361b79e14a2b
WenheLI <[wl1508@nyu.edu]>
2018-07-22 04:15:54 +0800

    add

--- routes/news.js
+++ routes/news.js
@@ -30,14 +30,10 @@ router.get('/news_map', (req, res, next) => {
     r = 100;
     scale = 10;
     searchDB({}, (res) => {
-        console.log(res);
         res.forEach((it) => {
             ans.push(it);
         });
-        console.log(ans);
-
     });
-    console.log(ans);
     setTimeout(() => {res.json(ans)}, 2000);
 });
 
